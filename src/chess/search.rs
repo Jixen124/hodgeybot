f8557d0f8e02775736eab245d5eb781c933fe9fd
Jixen124 <120450640+Jixen124@users.noreply.github.com>
2024-07-25 20:52:46 +1000

    Improved search.

--- src/chess/search.rs
+++ src/chess/search.rs
@@ -4,23 +4,33 @@ mod piece_square_tables;
 mod test_fens;
 
 const INFINITY: i16 = i16::MAX;
-const NEG_INFINITY: i16 = i16::MIN +1;
+const NEG_INFINITY: i16 = -INFINITY;
 const TRANSPOSITION_TABLE_SIZE: usize = 1024 * 1024 * 8;
 const TABLE_INDEX_MASK: usize = TRANSPOSITION_TABLE_SIZE - 1;
+
+#[derive(Clone, Copy, PartialEq)]
+enum TranspositionTableFlag {
+    Exact,
+    Lowerbound,
+    Upperbound,
+    None //Not really needed
+}
 
 #[derive(Clone, Copy)]
 struct TranspositionTableData {
     hash: u64,
     score: i16,
-    depth: u16
+    depth: u16,
+    flag: TranspositionTableFlag
 }
 
 pub fn find_best_move(chess: &Chess, depth: u16) -> Move {
-    let moves = chess.legal_moves();
+    let mut moves = chess.legal_moves();
+    moves.sort_unstable_by_key(|m| move_score(m));
     let mut best_score = NEG_INFINITY;
     let mut best_move = None;
 
-    let mut transposition_table: Vec<TranspositionTableData> = vec![TranspositionTableData{hash: 0, score: 0, depth: 0}; TRANSPOSITION_TABLE_SIZE];
+    let mut transposition_table: Vec<TranspositionTableData> = vec![TranspositionTableData{hash: 0, score: 0, depth: 0, flag: TranspositionTableFlag::None}; TRANSPOSITION_TABLE_SIZE];
     for m in moves {
         let mut new_chess = chess.clone();
         new_chess.play_unchecked(&m);
@@ -39,14 +49,7 @@ pub fn find_best_move(chess: &Chess, depth: u16) -> Move {
     panic!("NO BEST MOVE");
 }
 
-fn nega_max(chess: &Chess, depth: u16, mut alpha: i16, beta: i16, transposition_table: &mut Vec<TranspositionTableData>) -> i16 {
-    let hash: Zobrist64 = chess.zobrist_hash(shakmaty::EnPassantMode::Legal);
-    let table_index = hash.0 as usize & TABLE_INDEX_MASK;
-    if transposition_table[table_index].hash == hash.0 {
-        return transposition_table[table_index].score;
-    }
-    
-    //Confirm this works
+fn nega_max(chess: &Chess, depth: u16, mut alpha: i16, mut beta: i16, transposition_table: &mut Vec<TranspositionTableData>) -> i16 {
     if let Some(outcome) = chess.outcome() {
         return match outcome {
             Outcome::Draw => 0,
@@ -57,11 +60,30 @@ fn nega_max(chess: &Chess, depth: u16, mut alpha: i16, beta: i16, transposition_
     if depth == 0 {
         return evaluate_position(chess.board()) * if chess.turn().is_white() {1} else {-1};
     }
+    
+    let original_alpha = alpha;
+
+    let hash: Zobrist64 = chess.zobrist_hash(shakmaty::EnPassantMode::Legal);
+    let table_index = hash.0 as usize & TABLE_INDEX_MASK;
+    if transposition_table[table_index].hash == hash.0 && transposition_table[table_index].depth >= depth {
+        if transposition_table[table_index].flag == TranspositionTableFlag::Exact {
+            return transposition_table[table_index].score;
+        }
+        else if transposition_table[table_index].flag == TranspositionTableFlag::Lowerbound {
+            alpha = alpha.max(transposition_table[table_index].score);
+        }
+        else if transposition_table[table_index].flag == TranspositionTableFlag::Upperbound {
+            beta = beta.min(transposition_table[table_index].score);
+        }
+        
+        if alpha >= beta {
+            return transposition_table[table_index].score;
+        }
+    }
 
     let mut value = NEG_INFINITY;
 
     let mut moves = chess.legal_moves();
-    
     moves.sort_unstable_by_key(|m| move_score(m));
 
     for m in &moves {
@@ -71,7 +93,7 @@ fn nega_max(chess: &Chess, depth: u16, mut alpha: i16, beta: i16, transposition_
         value = value.max(score);
         alpha = alpha.max(value);
         if alpha >= beta {
-            return beta;
+            break;
         }
     }
 
@@ -79,6 +101,15 @@ fn nega_max(chess: &Chess, depth: u16, mut alpha: i16, beta: i16, transposition_
         transposition_table[table_index].hash = hash.0;
         transposition_table[table_index].score = value;
         transposition_table[table_index].depth = depth;
+        transposition_table[table_index].flag = if value <= original_alpha {
+            TranspositionTableFlag::Upperbound
+        }
+        else if value >= beta {
+            TranspositionTableFlag::Lowerbound
+        }
+        else {
+            TranspositionTableFlag::Exact
+        }
     }
     
     value
@@ -169,6 +200,6 @@ mod tests {
     fn lasker_position() {
         let setup = Fen::from_ascii("8/k7/3p4/p2P1p2/P2P1P2/8/8/K7 w - -".as_bytes()).expect("Fen should be valid").0;
         let chess = Chess::from_setup(setup, CastlingMode::Standard).expect("position should be valid");
-        assert!(super::find_best_move(&chess, 100).to_string() == "Ka1-b1");
+        assert!(super::find_best_move(&chess, 20).to_string() == "Ka1-b1");
     }
 }
