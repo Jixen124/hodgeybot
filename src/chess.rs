1e19bb8e4dc45b4383f7436ddbc1a5874077e544
Jixen124 <120450640+Jixen124@users.noreply.github.com>
2024-09-23 19:35:47 +1000

    Moved chess logic elsewhere.

--- src/chess.rs
+++ src/chess.rs
@@ -1,10 +1,11 @@
+use std::time::Duration;
 use shakmaty::san::{ParseSanError, San, SanError};
+use shakmaty::zobrist::{Zobrist64, ZobristHash};
 use shakmaty::{Bitboard, Chess, Color, Move, Position};
 use shakmaty::uci::{IllegalUciMoveError, UciMove};
 use serenity::prelude::*;
 use rand::{Rng, thread_rng};
-
-mod search;
+use hodgey_chess_engine::find_best_move_with_time;
 
 pub struct ChessGames;
 
@@ -42,6 +43,7 @@ pub struct ChessGame {
     chess: Chess,
     pub show_coordinates: bool,
     pub board_flips: bool,
+    previously_seen_hashes: Vec<u64>,
 }
 
 impl ChessGame {
@@ -53,6 +55,7 @@ impl ChessGame {
                 chess: Chess::default(),
                 show_coordinates: true,
                 board_flips: false,
+                previously_seen_hashes: Vec::new(),
             }
         }
         else {
@@ -62,6 +65,7 @@ impl ChessGame {
                 chess: Chess::default(),
                 show_coordinates: true,
                 board_flips: false,
+                previously_seen_hashes: Vec::new(),
             }
         }
     }
@@ -79,6 +83,8 @@ impl ChessGame {
 
     pub fn make_move_unchecked(&mut self, selected_move: Move) {
         self.chess.play_unchecked(&selected_move);
+        let new_hash = self.chess.zobrist_hash::<Zobrist64>(shakmaty::EnPassantMode::Legal).0;
+        self.previously_seen_hashes.push(new_hash);
     }
 
     pub fn legal_move_from_string(&self, mut move_string: String) -> Result<Move, MoveError> {
@@ -94,8 +100,8 @@ impl ChessGame {
         Ok(legal_move)
     }
 
-    pub fn find_best_move(&self) -> Move {
-        search::find_best_move(&self.chess, 6)
+    pub fn find_best_move(&mut self) -> Move {
+        find_best_move_with_time(&self.chess, Duration::from_secs(1), &mut self.previously_seen_hashes)
     }
 
     pub fn is_in_check(&self) -> bool {
@@ -118,7 +124,7 @@ impl ChessGame {
     }
 
     pub fn gameover(&self) -> bool {
-        self.chess.is_game_over()
+        self.chess.is_game_over() || (self.chess.halfmoves() > 100)
     }
 
     pub fn get_gameover_message(&self) -> &'static str {        
@@ -127,6 +133,9 @@ impl ChessGame {
         }
         else if self.chess.is_insufficient_material() {
             "Stalemate! Insufficient material."
+        }
+        else if self.chess.halfmoves() > 100 {
+            "Draw by 50 move rule!"
         }
         else {
             "Stalemate!"
